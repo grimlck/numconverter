4843a1df46f6b81dd85148b7168cfac9482d6844
Micha <michael@swarrrm.de>
2012-08-07 18:20:04 +0200

    Completed dec to bin

--- numconverter.py
+++ numconverter.py
@@ -3,17 +3,17 @@ def dec_to_bin(dec):
     result = dec / 2
     rest = dec % 2
     print "%s : 2 = %s -- Rest: %s" % (dec,int(result),rest)
-    return (int(result),str(rest))
+    if int(result) != 0:
+        binary = str(rest) + str(dec_to_bin(int(result)))
+        return binary
+    else:
+        return str(rest)
 def main():
     number = 12345
-    binary=""
-    while True:
-        number,rest = dec_to_bin(number)
-        if rest != 0:
-            binary = binary+rest
-        else:
-            break
+    binary = dec_to_bin(number)
     print binary
+    print binary[::-1]
+    
 
 if __name__ == "__main__":
     main()
