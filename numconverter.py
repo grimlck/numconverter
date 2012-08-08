ef5a2096e2c2a27f221592e4e03071e19724cfd1
Micha <michael@swarrrm.de>
2012-08-08 21:17:36 +0200

    Added hex to dec function

--- numconverter.py
+++ numconverter.py
@@ -38,14 +38,49 @@ def bin_to_dec(binary):
 
     return decimal
 
+def hex_to_dec(hexa):
+    substitution = {
+        "a" : "10",
+        "b" : "11",
+        "c" : "12",
+        "d" : "13",
+        "e" : "14",
+        "f" : "15"
+    }
+    """
+    For further version, following code would inverse a decitionary, even when the values
+    are not unique:
+    inv_substitution = {}
+    for k, v in substitution.iteritems():
+        inv_substitution[v] = inv_substitution.get(v, [])
+        inv_substitution[v].append(k)
+    
+    (found here: http://stackoverflow.com/questions/483666/python-reverse-inverse-a-mapping)
+    """
+
+    decimal = 0
+    rev_hex = str(hexa)[::-1]
+    for digit in range(len(rev_hex)):
+        if rev_hex[digit].lower() in substitution:
+            new_hex = rev_hex[digit].replace(rev_hex[digit],substitution[rev_hex[digit]])
+        else:
+            new_hex = rev_hex[digit]
+        
+        decimal = decimal + int(new_hex) * (16 ** digit)
+
+    return decimal
+
 def main():
-    number = 123455
+    number = 15
     binary_input = 1111
+    hex_input = "ff"
     binary = dec_to_bin(number)
     print "dec: %s = bin: %s\n" % (number,binary[::-1])
     hexa = dec_to_hex(number)
     print "dec: %s = hex: %s\n" % (number,hexa[::-1])
     print "bin: %s = dec: %s\n" % (binary_input, bin_to_dec(binary_input))
+    print "hex: %s = dec: %s\n" % (hex_input, hex_to_dec(hex_input))
+
 
 if __name__ == "__main__":
     main()
